cccfd75d7c1f22e49f26e2f921cd900939f7009c
Veronika Darst <concitta@yandex.ru>
2024-08-22 20:18:12 +0300

    Add files via upload

---
+++ module2_5.py
@@ -0,0 +1,22 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in matrix:
+        matrix1 = []
+        i = matrix1 * n
+
+        for j in matrix1:
+            j = value * m
+            j.append.matrix1()
+        i.append.matrix()
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+# [[10, 10], [10, 10]]
+# [[42, 42, 42, 42, 42], [42, 42, 42, 42, 42], [42, 42, 42, 42, 42]]
+# [[13, 13], [13, 13], [13, 13], [13, 13]]
